6cedf6f7fa0dc0ea5d77f2f5ec4be039823b8ab1
Ever-VC <ever.vasquezc27@gmail.com>
2026-01-27 20:17:47 -0600

    :sparkles: feat(controllers/FooController | interceptors/LoadingTimeInterceptor): Agregando FooController y actualizando interceptor para loguear el nombre del método

--- src/main/java/com/evervc/springboot/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
+++ src/main/java/com/evervc/springboot/interceptor/springbootinterceptor/interceptors/LoadingTimeInterceptor.java
@@ -6,6 +6,7 @@ import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,12 +18,14 @@ public class LoadingTimeInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        logger.info("LoadingTimeInterceptor: preHandle() entrando...");
+        HandlerMethod controller = (HandlerMethod) handler;
+        logger.info("LoadingTimeInterceptor: preHandle() entrando a [" + controller.getMethod().getName() + "]");
         return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
-        logger.info("LoadingTimeInterceptor: postHandle() saliendo...");
+        HandlerMethod controller = (HandlerMethod) handler;
+        logger.info("LoadingTimeInterceptor: postHandle() saliendo de [" + controller.getMethod().getName() + "]");
     }
 }
